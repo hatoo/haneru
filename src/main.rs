098e8fd5748dcad18f671706bf6c667dd4f76c27
hatoo <hato2000@gmail.com>
2023-10-22 15:20:15 +0900

    proxy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use askama::Template;
 use axum::{routing::get, Router};
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Client, Request, Response, Server,
+};
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -11,8 +15,9 @@ async fn main() {
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
     println!("Listening on http://{}/", addr);
+    tokio::spawn(run_proxy());
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -26,4 +31,23 @@ struct Index;
 // basic handler that responds with a static string
 async fn root() -> Index {
     Index
+}
+
+async fn proxy(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let client = Client::new();
+    client.request(req).await
+}
+
+async fn run_proxy() -> anyhow::Result<()> {
+    let addr = ([127, 0, 0, 1], 3002).into();
+
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(proxy)) });
+
+    let server = Server::bind(&addr).serve(service);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
